db3d467972ca3642df4b61deb13da8ad730a7990
Fatih Metiner <fatihmetiner@outlook.com>
2023-02-22 12:30:58 +0300

    Add project files.

---
+++ PhotoGallery/Models/Resim.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoGallery.Models
+{
+    public class Resim
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Baslik { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        [Required]
+        public string DosyaAd { get; set; } = string.Empty;
+    }
+}

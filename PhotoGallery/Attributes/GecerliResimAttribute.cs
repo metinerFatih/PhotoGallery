70911fffa13b85c68218824f6bdb4e758b52d0d7
Fatih Metiner <fatihmetiner@outlook.com>
2023-02-23 12:39:27 +0300

    file_upload_validation_CustomAttribute

---
+++ PhotoGallery/Attributes/GecerliResimAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoGallery.Attributes
+{
+    public class GecerliResimAttribute : ValidationAttribute
+    {
+        public int MaksDosyaBoyutuMb { get; set; }
+
+        public override bool IsValid(object? value)
+        {
+            IFormFile? file = value as IFormFile;
+
+            if (file == null)
+                return true;
+
+            if (MaksDosyaBoyutuMb > 0 && file.Length > MaksDosyaBoyutuMb * 1024 * 1024)
+            {
+                ErrorMessage = $"Dosya boyutu{MaksDosyaBoyutuMb} MB'dan büyük";
+                return false;
+            }
+            else if (file.ContentType.StartsWith("image/"))
+            {
+                ErrorMessage = "Geçersiz resim formatı.";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- PhotoGallery/Models/ResimViewModel.cs
+++ PhotoGallery/Models/ResimViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using PhotoGallery.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhotoGallery.Models
 {
     public class ResimViewModel
     {
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage ="{0} alanı en fazla {1} karakterden oluşabilir.")]
+        [Required(ErrorMessage ="{0} alanı zorunludur.")]
+        [Display(Name = "Başlık")]
         public string Baslik { get; set; } = string.Empty;
-
+        [GecerliResim(MaksDosyaBoyutuMb = 1)]
+        [Display(Name = "Resim")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
         public IFormFile Dosya { get; set; } = null!;
+
+        public List<Resim> Resimler { get; set; } = new();
     }
 }
